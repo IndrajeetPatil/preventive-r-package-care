eb4a22a20e860ba09e343c364a2253d464513c25
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-26 12:55:41 +0200

    explicitly specify `output_format`

--- renderer.R
+++ renderer.R
@@ -1,5 +1,5 @@
 # if you want to build the slides locally
-quarto::quarto_render("slides.qmd")
+quarto::quarto_render("slides.qmd", output_format = "revealjs")
 
 # duplicate `slides.html` as `index.html`
 fs::file_copy("slides.html", "index.html", overwrite = TRUE)
