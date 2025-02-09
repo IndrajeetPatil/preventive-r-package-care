13850692028ac79fb6d531ee5fac1f0709a2d79b
Indrajeet Patil <patilindrajeet.science@gmail.com>
2025-02-09 14:25:51 +0100

    rename slides to index

--- renderer.R
+++
@@ -1,8 +0,0 @@
-# if you want to build the slides locally
-quarto::quarto_render("slides.qmd", output_format = "revealjs")
-
-# duplicate `slides.html` as `index.html`
-fs::file_copy("slides.html", "index.html", overwrite = TRUE)
-
-# also rebuild README
-rmarkdown::render("README.Rmd")
